f7bf792a37bcbc5b66457f6df55190cbd4f910d9
Thomas Adriaanse <18tma6@queensu.ca>
2022-10-28 16:14:14 -0400

    draft setup

--- main.py
+++ main.py
@@ -1,18 +1,91 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg#, NavigationToolbar2TkAgg
 from matplotlib.animation import FuncAnimation
-import greedyAlgorithm
+from matplotlib.figure import Figure
+
 import random
 
 import greedyAlgorithm
 import nodeStructure as NS
 
+import tkinter as tk
+from tkinter import ttk
+
+
+greedyGraph = Figure(figsize=(5,5), dpi=100)
+a = greedyGraph.add_subplot(111)
+
+class TravellingSalespersonApp(tk.Tk):
+
+    def __init__(self, *args, **kwargs):
+        
+        tk.Tk.__init__(self, *args, **kwargs)
+
+        #tk.Tk.iconbitmap(self, default=folderLocation+"FantasyScraperIcon.ico")
+        tk.Tk.wm_title(self, "Travelling Salesperson")
+        
+        #frame1 = SelectPage(container, self)
+        
+
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand = True)
+
+        
+        container.grid_rowconfigure(0, weight=100)
+        #container.grid_rowconfigure(1, weight=100)
+        container.grid_columnconfigure(0, weight=100)
+
+        
+
+        self.frames = {}
+
+        
+
+        for F in (SelectPage, GreedyPage):
+            
+            frame = F(container, self)
+            self.frames[F] = frame
+            frame.pack(expand=TRUE)
+            #frame.grid(row=1, column=0, sticky="nsew")
+
+        self.show_frame(SelectPage)
+        #self.show_frame(SelectPage)
+
+    def show_frame(self, cont):
+
+        frame = self.frames[cont]
+        frame.tkraise()
+
+
+class SelectPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self,parent)
+
+        button1 = ttk.Button(self, text="Agree",
+                            command=lambda: [controller.show_frame(GreedyPage), controller.show_frame(SelectPage)])
+        button1.pack(side=LEFT, anchor=NE)
+
+        button2 = ttk.Button(self, text="Agree2",
+                            command=lambda: controller.show_frame(GreedyPage))
+        button2.pack(side=LEFT, anchor=NE)
+
+
+class GreedyPage(ttk.Frame):
+    def __init__(self, container):
+        super().__init__(container)
+        # setup the grid layout manager
+
+        canvas = FigureCanvasTkAgg(greedyGraph, self)
+        canvas.draw()
+        canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
 
 
 #animation function used to colour points
 def animate(i):
     #plt.style.use('seaborn')
+    greedyAlgorithm.greedy(plt)
     plt.scatter(NS.xVals, NS.yVals, c=NS.colours, cmap='summer',
                 edgecolor='black', alpha=0.75)
 
@@ -39,8 +112,8 @@ while i < NS.numNodes - 1:
     i += 1
 
 #call animation to change colours of nodes
-ani = FuncAnimation(plt.gcf(), animate, interval = 300)
+ani = FuncAnimation(greedyGraph, animate, interval = 300)
 
 greedyAlgorithm.greedy(plt)
-
+TravellingSalespersonApp.mainloop()
 #plt.show()
